76c58326ee9c3e97b2c7f4981bd535aca93dfeee
Stephen-Li-ICD2O1-01 <stephen.li@student.tdsb.on.ca>
2026-04-14 15:42:05 +0000

    save

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,57 @@
+/*
+File: Lesson 4.5 - Counters, Accumulators and Nesting
+Author: Stephen Li
+Date Created: April 14, 2026
+Date Last Modified: April 14Z, 2026
+*/
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
 
-	
+	public static String triangle(int num1) {
+		String triangle = "";
+		for (int i = 0; i < num1; i++) {
+			for (int j = 0; j <= i; j++) {
+				triangle += "*";
+			}
+			triangle += "\n";
+		}
+		return triangle;
+	}
+	 
+	public static String pyramid(int num1) {
+		String pyramid = "";
+		for (int i = 0; i < num1; i++) {
+			for (int j = 0; j < num1 - i - 1; j++) {
+				pyramid += " ";
+			}
+			for (int j = 0; j <= i; j++) {
+				pyramid += "*";
+				if (j < i) {
+					pyramid += " ";
+				}
+			}
+			pyramid += "\n";
+		}
+		return pyramid;
+	}
 
+	public static String multiplicationTable(int num1) {
+		String table = "";
+		String row = "";
+		for (int i = 1; i <= num1; i++) {
+			for (int j = 1; j <= num1; j++) {
+				row += i * j;
+				if (j != num1) {
+					row += " ";
+				}
+			}
+			table += row + "\n";
+			row = "";
+		}
+		return table;
+	}
 }
